404b93f7b918c2a5871a933edcc22cf7794521c0
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-03-21 11:10:30 -0600

    UPDATE

---
+++ RECURSOS-INVESTIGACION/NOTAS/simulacion-sobre-modelos-entrenados/simulacion-arima.R
@@ -0,0 +1,17 @@
+library(fpp2)
+
+fit <- auto.arima(sunspotarea)
+
+nSim <- 3L
+h <- 20L
+
+sim <- ts(matrix(0, nrow=h, ncol=nSim),
+          start=end(sunspotarea)[1L]+1L)
+
+for(i in seq(nSim)){
+    sim[,i] <- simulate(fit, nsim=h)
+}
+
+autoplot(sunspotarea) + 
+    autolayer(forecast(fit,h=h), series = 'proyeccion') +
+    autolayer(sim)
