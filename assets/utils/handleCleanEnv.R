1b7c8500d1dca24d309bdedbe2daee70a1f2cd32
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-03-21 11:33:21 -0600

    UPDATE

---
+++ assets/utils/handleCleanEnv.R
@@ -0,0 +1,11 @@
+if (!('utils' %in% ls())) { utils <- list() }
+
+# LIMPIAR MEMORIA
+
+noVar <- c('utils', 'simbolos')
+rm(list =ls()[!(ls() %in% noVar)])
+
+utils$handleCleanEnv <- function(){
+    source('assets/utils/handleCleanEnv.R')
+    print('Claer Env')
+}
