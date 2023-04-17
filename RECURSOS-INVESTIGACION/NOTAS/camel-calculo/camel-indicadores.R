49e2fe108f50ff3159bf3c97524784c55e78ec9d
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-04-17 12:44:37 -0600

    UPDATE

--- RECURSOS-INVESTIGACION/NOTAS/camel-calculo/camel-indicadores.R
+++ RECURSOS-INVESTIGACION/NOTAS/camel-calculo/camel-indicadores.R
@@ -16,7 +16,11 @@ dat$indCap_CCCM <- INDICADORES_CAMEL$indCap_CCCM(cartVnc = dat$ACTIVO_CARTERA_CA
                                                  prevCart = dat$ACTIVO_CARTERA_PREVISION_PARA_INCOBRABILIDAD_DE_CARTERA,
                                                  patrimonio = dat$PATRIMONIO)
 
-
+dat$indCap_CACCM <- INDICADORES_CAMEL$indCap_CACCM(cartVnc = dat$ACTIVO_CARTERA_CARTERA_VENCIDA_TOTAL,
+                                                   cartEjc = dat$ACTIVO_CARTERA_CARTERA_EJECUCION_TOTAL,
+                                                   prevCart = dat$ACTIVO_CARTERA_PREVISION_PARA_INCOBRABILIDAD_DE_CARTERA,
+                                                   realizables = dat$ACTIVO_BIENES_REALIZABLES,
+                                                   patrimonio = dat$PATRIMONIO)
 
 
 INDICADORES_CAMEL <- list()
@@ -30,7 +34,16 @@ INDICADORES_CAMEL$indCap_CCCM <- function(cartVnc=NA, cartEjc=NA, prevCart=NA, p
     return(result)
 }
 
+# Coeficiente Ácido De Cobertura De Cartera En Mora
+INDICADORES_CAMEL$indCap_CACCM <- function(cartVnc=NA, cartEjc=NA, prevCart=NA, realizables=NA, patrimonio=NA) {
+    result <- ((cartVnc+cartEjc)-abs(prevCart)+realizables)/patrimonio
+    return(result)
+}
 
+
+
+
+######################################################
 tsDat <- ts(matrix(0, nrow=(2022-2021+1)*12, ncol=length(unique(dat$TIPO_DE_ENTIDAD))), 
             start=2021, frequency=12)
 
@@ -38,10 +51,10 @@ for (i in 1:length(unique(dat$TIPO_DE_ENTIDAD))) {
     
     tipoEnt <- as.character(unique(dat$TIPO_DE_ENTIDAD)[i])
     colnames(tsDat)[i] <- tipoEnt
-    x <- dat[dat$TIPO_DE_ENTIDAD==tipoEnt,][order(dat[dat$TIPO_DE_ENTIDAD==tipoEnt,'FECHA']),'indCap_CCCM']
+    x <- dat[dat$TIPO_DE_ENTIDAD==tipoEnt,][order(dat[dat$TIPO_DE_ENTIDAD==tipoEnt,'FECHA']),'ACTIVO']
     x <- sapply(x, as.numeric)
     tsDat[,i] <- x
 }
 
-autoplot(tsDat)
+autoplot(tsDat[,-2])
 
