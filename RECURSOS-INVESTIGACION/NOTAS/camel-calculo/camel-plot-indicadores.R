d55788ada46919e21fa20c0e371a606d287fe6f2
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-04-18 12:36:00 -0600

    UPDATE

--- RECURSOS-INVESTIGACION/NOTAS/camel-calculo/camel-plot-indicadores.R
+++ RECURSOS-INVESTIGACION/NOTAS/camel-calculo/camel-plot-indicadores.R
@@ -22,3 +22,23 @@ for (i in 1:length(unique(dat$TIPO_DE_ENTIDAD))) {
 }
 
 autoplot(tsDat)
+
+
+plotIndCamel <- function(id, dat) {
+    gestInc <- min(as.numeric(format(dat$FECHA, format='%Y')))
+    gestFn <- max(as.numeric(format(dat$FECHA, format='%Y')))
+    
+    tsDat <- ts(matrix(0, nrow=(gestFn-gestInc+1)*12, ncol=length(unique(dat$TIPO_DE_ENTIDAD))), 
+                start=gestInc, frequency=12)
+    
+    for (i in 1:length(unique(dat$TIPO_DE_ENTIDAD))) {
+        
+        tipoEnt <- as.character(unique(dat$TIPO_DE_ENTIDAD)[i])
+        colnames(tsDat)[i] <- tipoEnt
+        x <- dat[dat$TIPO_DE_ENTIDAD==tipoEnt,][order(dat[dat$TIPO_DE_ENTIDAD==tipoEnt,'FECHA']),id]
+        x <- sapply(x, as.numeric)
+        tsDat[,i] <- x
+    }
+    
+    return(tsDat)
+}
