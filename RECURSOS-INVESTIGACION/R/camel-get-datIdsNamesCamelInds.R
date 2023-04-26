7b9c0bdb16ef320f00dfcd7a83d57afb4d000380
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-04-26 14:59:19 -0600

    UPDATE

---
+++ RECURSOS-INVESTIGACION/R/camel-get-datIdsNamesCamelInds.R
@@ -0,0 +1,68 @@
+getDatIdsNamesCamelInds <- function() {
+    
+    indicadoresCamelId <- data.frame(
+        INDICADOR = c('indCap_CAP',        
+                      'indCap_CCCM',
+                      'indCap_CACCM',
+                      'indCap_CCP',
+                      
+                      'indAct_CEC',
+                      'indAct_CPC',
+                      'indAct_CPCM',
+                      'indAct_CRC',
+                      
+                      'indAdm_CCGA',
+                      'indAdm_CACGA',
+                      
+                      'indBenf_ROA',
+                      'indBenf_ROE',
+                      
+                      'indLq_CCPP',
+                      'indLq_CACPP'
+        ),
+        NOMBRES = c('Coeficiente de adecuación patrimonial',        
+                      'Coeficiente de cobertura de cartera en mora',
+                      'Coeficiente acido de cobertura de cartera en mora',
+                      'Coeficiente de cobertura patrimonial',
+                      
+                      'Coeficiente de exposición de cartera',
+                      'Coeficiente de previsión de cartera',
+                      'Coeficiente de previsión de cartera en mora',
+                      'Coeficiente de reposición de cartera',
+                      
+                      'Coeficiente de cobertura gastos administrativos',
+                      'Coeficiente acido de cobertura patrimonial',
+                      
+                      'Coeficiente de rendimiento sobre activos',
+                      'Coeficiente de rendimiento sobre patrimonio',
+                      
+                      'Coeficiente de capacidad de pago frente a pasivos',
+                      'Coeficiente acido de capacidad de pago frente a pasivos'
+        ),
+        DIRECCION = c('ASCENDENTE',        
+                      'DESCENDENTE',
+                      'DESCENDENTE',
+                      'ASCENDENTE',
+                      
+                      'DESCENDENTE',
+                      'ASCENDENTE',
+                      'ASCENDENTE',
+                      'DESCENDENTE',
+                      
+                      'DESCENDENTE',
+                      'DESCENDENTE',
+                      
+                      'ASCENDENTE',
+                      'ASCENDENTE',
+                      
+                      'ASCENDENTE',
+                      'ASCENDENTE'
+        )
+    )
+    
+    indicadoresCamelId$DECRECIENTE <- ifelse(indicadoresCamelId$DIRECCION=='ASCENDENTE', TRUE, FALSE)
+    
+    
+    return(indicadoresCamelId)
+    
+}
