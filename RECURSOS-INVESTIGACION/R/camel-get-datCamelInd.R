a4809292ad360aab55bf7ab8933839074b3290e9
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-05-07 01:35:29 -0600

    UPDATE

--- RECURSOS-INVESTIGACION/R/camel-get-datCamelInd.R
+++ RECURSOS-INVESTIGACION/R/camel-get-datCamelInd.R
@@ -9,11 +9,32 @@ getDatCamelInd <- function() {
     # Carga de datos
     dat <- read.xlsx('../../FUENTES-DE-DATOS/ASFI/ConsoleAppPrepararDatos/bin/Debug/DATOS_ASFI/BBDD_ESTADOS_FINANCIEROS.xlsx') 
     
+    # Ajustes previos
     dat$FECHA <- convertToDate(dat$FECHA)
     dat$TIPO_DE_ENTIDAD <- gsub('COPERATIVAS_DE_AHORRO_Y_CREDITO',
                                 'COOPERATIVAS_DE_AHORRO_Y_CREDITO',
                                 dat$TIPO_DE_ENTIDAD)
     
+    dat$GESTION <- as.character(dat$GESTION)
+    dat$MES <- as.character(dat$MES)
+    dat$DIA <- as.character(dat$DIA)
+    
+    datEMB <- dat %>% 
+        filter(EXPRESADO=='EN_MILES_DE_BOLIVIANOS') %>% 
+        mutate(across(.cols = where(is.numeric), ~ .x * 1000 ))
+    
+    datEB <- dat %>% 
+        filter(EXPRESADO=='EN_BOLIVIANOS')
+    
+    dat <- bind_rows(datEMB,datEB)
+    
+    dat$EXPRESADO <- rep('EN_BOLIVIANOS',nrow(dat))
+    
+    dat$GESTION <- as.numeric(dat$GESTION)
+    dat$MES <- as.numeric(dat$MES)
+    dat$DIA <- as.numeric(dat$DIA)
+    
+    # Agrupar por sectores
     dat <- dat %>% group_by(TIPO_DE_ENTIDAD, FECHA) %>% summarise_if(is.numeric, sum)
     dat <- dat[ , !(names(dat) %in% c('GESTION','MES','DIA'))]
     
---
+++ RECURSOS-INVESTIGACION/R/get-dat-basic.R
@@ -0,0 +1,41 @@
+getDatEEFF <- function() {
+    
+    require(openxlsx)
+    
+    rootMainDir <- 'E:/INVESTIGACION-PARA-OBTENER-EL-GRADO-DE-LICENCIATURA-EN-INGENIERIA-FINANCIERA-UMSS'
+    rootFile <- 'FUENTES-DE-DATOS/ASFI/ConsoleAppPrepararDatos/bin/Debug/DATOS_ASFI/BBDD_ESTADOS_FINANCIEROS.xlsx'
+    
+    if (getwd() == rootMainDir ) {
+        dat <- read.xlsx(rootFile) 
+    }else{
+        dat <- read.xlsx(paste0('../../',rootFile) ) 
+    }
+    
+    dat$FECHA <- convertToDate(dat$FECHA)
+    
+    dat$TIPO_DE_ENTIDAD <- gsub('COPERATIVAS_DE_AHORRO_Y_CREDITO',
+                                'COOPERATIVAS_DE_AHORRO_Y_CREDITO',
+                                dat$TIPO_DE_ENTIDAD)
+    
+    dat$GESTION <- as.character(dat$GESTION)
+    dat$MES <- as.character(dat$MES)
+    dat$DIA <- as.character(dat$DIA)
+    
+    datEMB <- dat %>% 
+        filter(EXPRESADO=='EN_MILES_DE_BOLIVIANOS') %>% 
+        mutate(across(.cols = where(is.numeric), ~ .x * 1000 ))
+    
+    datEB <- dat %>% 
+        filter(EXPRESADO=='EN_BOLIVIANOS')
+    
+    dat <- bind_rows(datEMB,datEB)
+    
+    dat$EXPRESADO <- rep('EN_BOLIVIANOS',nrow(dat))
+    
+    dat$GESTION <- as.numeric(dat$GESTION)
+    dat$MES <- as.numeric(dat$MES)
+    dat$DIA <- as.numeric(dat$DIA)
+    
+    return(dat)
+    
+}
