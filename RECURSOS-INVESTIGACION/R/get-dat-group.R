206f30c90b29bb35930c8db0f3f3f572e4a64bc4
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-05-12 11:47:59 -0600

    UPDATE

---
+++ RECURSOS-INVESTIGACION/R/get-dat-group.R
@@ -0,0 +1,19 @@
+getDatEEFFByGroup <- function(dat=NULL, by='TIPO_DE_ENTIDAD') {
+    
+    # Paquetes necesarios
+    require(dplyr)
+    
+    # Verificar si "dat" es nulo
+    if (is.null(dat) ) {
+        source('RECURSOS-INVESTIGACION/R/get-dat-basic.R')
+        dat <- getDatEEFF()   
+    }
+    
+    # Agrupar por sectores
+    if (by=='TIPO_DE_ENTIDAD') {
+        dat <- dat %>% group_by(TIPO_DE_ENTIDAD, FECHA) %>% summarise_if(is.numeric, sum)
+        dat <- dat[ , !(names(dat) %in% c('GESTION','MES','DIA'))]   
+    }
+    
+    return(dat)
+}
