2a855b17e96ac1bf7d7825c888546163ecc654c8
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-05-22 23:09:02 -0600

    UPDATE

--- RECURSOS-INVESTIGACION/005-DETERMINACION-REDES-NEURONALES-PRONOSTICOS/prueba.R
+++ RECURSOS-INVESTIGACION/005-DETERMINACION-REDES-NEURONALES-PRONOSTICOS/prueba.R
@@ -13,25 +13,26 @@ dat <-
     group_by(FECHA) %>% 
     summarise_if(is.numeric, sum)
 
-gestionFn <- max(dat$FECHA) - years(1)
+gestionFn <- max(dat$FECHA) - years(1) + 4
 
 datTrain <- dat %>% filter(FECHA<gestionFn)
 datTest <- dat %>% filter(FECHA>gestionFn)
 
-id <- 'ACTIVO'
+id <- 'EERR_S2_GASTOS_DE_ADMINISTRACION'
 tsDatTrain <- getTsFromDat2(id,datTrain)
 tsDatTest <- getTsFromDat2(id,datTest)
 
 mcoModel <- tslm(tsDatTrain~trend)
 nnModel <- nnetar(tsDatTrain)
 
-mcoForecast <- forecast(mcoModel, h = 13)
-nnForecast <- forecast(nnModel,h = 13)
+mcoForecast <- forecast(mcoModel, h = 12)
+nnForecast <- forecast(nnModel,h = 12)
 
 autoplot(tsDatTrain) + 
     autolayer(mcoForecast, PI = F, series = 'MCO') + 
     autolayer(nnForecast, series = 'NN')  + 
     autolayer(tsDatTest)
 
-
+r2_nn <- (cor(nnForecast$mean, tsDatTest))^2
+r2_mco <- (cor(mcoForecast$mean, tsDatTest))^2
 
