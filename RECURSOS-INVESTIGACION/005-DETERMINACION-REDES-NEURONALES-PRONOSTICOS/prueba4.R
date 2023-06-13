ce97f86a1c775a01aec17ea5303aa229cffe347d
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-06-13 15:38:00 -0600

    UPDATE

--- RECURSOS-INVESTIGACION/005-DETERMINACION-REDES-NEURONALES-PRONOSTICOS/prueba4.R
+++ RECURSOS-INVESTIGACION/005-DETERMINACION-REDES-NEURONALES-PRONOSTICOS/prueba4.R
@@ -1,5 +1,6 @@
 source('RECURSOS-INVESTIGACION/R/get-dat-basic-normalizada.R')
 source('RECURSOS-INVESTIGACION/R/pef-r2-ids-eeff-forecast.R')
+source('RECURSOS-INVESTIGACION/R/pef-list-summary-resumen.R')
 
 datTotalSistema <- getDatEEFFNormalizada(by = 'TOTAL_SISTEMA')
 
@@ -13,38 +14,4 @@ ids <- c('ACTIVO',
          'EERR_S2_RESULTADO_NETO_DE_LA_GESTION')
 
 listResult <- getListFittedAndSimulateModels(datTotalSistema)
-
-# AJUSTE DE MODELO
-r2nnModelMean <-  sapply(listResult, function(x) x[['r2nnModel']]) %>% mean()
-r2mcoModelMean <-  sapply(listResult, function(x) x[['r2mcoModel']]) %>% mean() 
-r2arimaModelMean <-  sapply(listResult, function(x) x[['r2arimaModel']]) %>% mean()
-
-# AJUSTE DE SIMULAICON DE PROYECCIONES
-nnModelSimulateR2Mean <-  sapply(listResult, function(x) x[['nnModelSimulate']][['r2Mean']]) %>% mean()
-mcoModelSimulateR2Mean <-  sapply(listResult, function(x) x[['mcoModelSimulate']][['r2Mean']]) %>% mean()
-arimaModelSimulateR2Mean <-  sapply(listResult, function(x) x[['arimaModelSimulate']][['r2Mean']]) %>% mean()
-
-# DATAFRAMES RESUMENES
-r2ModelsResum <- data.frame(
-    CUENTAS = sapply(listResult, function(x) x[['r2mcoModel']]) %>% names(),
-    NN = sapply(listResult, function(x) x[['r2nnModel']]),
-    MCO = sapply(listResult, function(x) x[['r2mcoModel']]),
-    ARIMA = sapply(listResult, function(x) x[['r2arimaModel']])
-)
-
-r2ForecastResum <- data.frame(
-    CUENTAS = sapply(listResult, function(x) x[['mcoModelSimulate']][['r2Mean']]) %>% names(),
-    NN = sapply(listResult, function(x) x[['nnModelSimulate']][['r2Mean']]),
-    MCO = sapply(listResult, function(x) x[['mcoModelSimulate']][['r2Mean']]),
-    ARIMA = sapply(listResult, function(x) x[['arimaModelSimulate']][['r2Mean']])
-)
-
-r2ModelsResumMean <- data.frame(
-    MODELOS = c('NN','MCO','ARIMA'),
-    R2_PROMEDIO = c(r2nnModelMean, r2mcoModelMean, r2arimaModelMean) 
-)
-
-r2ForecastResumMean <- data.frame(
-    MODELOS = c('NN','MCO','ARIMA'),
-    R2_PROMEDIO = c(nnModelSimulateR2Mean, mcoModelSimulateR2Mean, arimaModelSimulateR2Mean) 
-)
+listResumeModels <- getListResumeSummaryModels(listResult)
---
+++ RECURSOS-INVESTIGACION/R/pef-list-summary-resumen.R
@@ -0,0 +1,52 @@
+getListResumeSummaryModels <- function(listResult) {
+    
+    require(dplyr)
+    
+    # AJUSTE DE MODELO
+    r2nnModelMean <-  sapply(listResult, function(x) x[['r2nnModel']]) %>% mean()
+    r2mcoModelMean <-  sapply(listResult, function(x) x[['r2mcoModel']]) %>% mean() 
+    r2arimaModelMean <-  sapply(listResult, function(x) x[['r2arimaModel']]) %>% mean()
+    
+    # AJUSTE DE SIMULAICON DE PROYECCIONES
+    nnModelSimulateR2Mean <-  sapply(listResult, function(x) x[['nnModelSimulate']][['r2Mean']]) %>% mean()
+    mcoModelSimulateR2Mean <-  sapply(listResult, function(x) x[['mcoModelSimulate']][['r2Mean']]) %>% mean()
+    arimaModelSimulateR2Mean <-  sapply(listResult, function(x) x[['arimaModelSimulate']][['r2Mean']]) %>% mean()
+    
+    # DATAFRAMES RESUMENES
+    r2ModelsResum <- data.frame(
+        CUENTAS = sapply(listResult, function(x) x[['r2mcoModel']]) %>% names(),
+        NN = sapply(listResult, function(x) x[['r2nnModel']]),
+        MCO = sapply(listResult, function(x) x[['r2mcoModel']]),
+        ARIMA = sapply(listResult, function(x) x[['r2arimaModel']]),
+        row.names = NULL
+    )
+    
+    r2ForecastResum <- data.frame(
+        CUENTAS = sapply(listResult, function(x) x[['mcoModelSimulate']][['r2Mean']]) %>% names(),
+        NN = sapply(listResult, function(x) x[['nnModelSimulate']][['r2Mean']]),
+        MCO = sapply(listResult, function(x) x[['mcoModelSimulate']][['r2Mean']]),
+        ARIMA = sapply(listResult, function(x) x[['arimaModelSimulate']][['r2Mean']]),
+        row.names = NULL
+    )
+    
+    r2ModelsResumMean <- data.frame(
+        MODELOS = c('NN','MCO','ARIMA'),
+        R2_PROMEDIO = c(r2nnModelMean, r2mcoModelMean, r2arimaModelMean) ,
+        row.names = NULL
+    )
+    
+    r2ForecastResumMean <- data.frame(
+        MODELOS = c('NN','MCO','ARIMA'),
+        R2_PROMEDIO = c(nnModelSimulateR2Mean, mcoModelSimulateR2Mean, arimaModelSimulateR2Mean) ,
+        row.names = NULL
+    )
+    
+    listResult2 <- list(r2ModelsResum=r2ModelsResum,
+                        r2ForecastResum=r2ForecastResum,
+                        r2ModelsResumMean=r2ModelsResumMean,
+                        r2ForecastResumMean=r2ForecastResumMean
+    )
+    
+    return(listResult2)
+    
+}
