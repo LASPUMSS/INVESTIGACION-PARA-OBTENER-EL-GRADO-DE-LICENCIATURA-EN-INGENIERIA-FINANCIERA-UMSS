52f0031964ad9aa81d9b48d277188ae9bd5e4b8e
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-10-21 10:33:18 -0600

    UPDATE

--- FUENTES-DE-DATOS/DATA/scripts/trainListResultPEF_Entidad.R
+++ FUENTES-DE-DATOS/DATA/scripts/trainListResultPEF_Entidad.R
@@ -3,13 +3,22 @@ source('RECURSOS-INVESTIGACION/R/pef-r2-ids-eeff-forecast.R')
 source('RECURSOS-INVESTIGACION/R/pef-get-ids-for-models.R')
 require(dplyr)
 
-entidad <- 'BME'
+siglasEntidades <- c('BCR','BEC','BFO','BFS','BGA','BIE','BIS','BME','BNA','BNB','BPR','BSO','BUN')
 
-datTotalSistema <- 
-    getDatEEFFNormalizada(by = 'ENTIDAD') %>% 
-    filter(TIPO_DE_ENTIDAD==entidad)
 
-ids <- getVariablesForModelsForecast()
-listResultPEF <- getListFittedAndSimulateModels(datTotalSistema,ids,n_simulaicones = 100L, printEnt = TRUE)
-saveRDS(listResultPEF, file =paste0('FUENTES-DE-DATOS/DATA/listResultPEF_', entidad, '.rds'))
+for (ent in siglasEntidades) {
+    
+    cat(paste0('\n', ent))
+    entidad <- ent
+    
+    datTotalSistema <- 
+        getDatEEFFNormalizada(by = 'ENTIDAD') %>% 
+        filter(TIPO_DE_ENTIDAD==entidad)
+    
+    ids <- getVariablesForModelsForecast()
+    listResultPEF <- getListFittedAndSimulateModels(datTotalSistema,ids,n_simulaicones = 100L, printEnt = TRUE)
+    saveRDS(listResultPEF, file =paste0('FUENTES-DE-DATOS/DATA/listResultPEF_', entidad, '.rds'))
+}
+
+
 
