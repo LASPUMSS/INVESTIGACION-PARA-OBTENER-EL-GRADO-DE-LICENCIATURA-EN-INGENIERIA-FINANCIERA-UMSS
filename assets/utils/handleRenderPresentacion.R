725b721cca390417ea74d08c08a97b91228ec1da
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-10-26 13:14:06 -0600

    UPDATE

--- assets/utils/handleRenderPresentacion.R
+++
@@ -1,8 +0,0 @@
-if (!('utils' %in% ls())) { utils <- list() }
-
-utils$handleRenderPresentacion<- function(){
-    
-    ruta <- paste0(getwd(), '/RECURSOS-INVESTIGACION/PRESENTACION/PRESENTACION.Rmd')
-    rmarkdown::render(ruta, knit_root_dir = getwd())
-    
-}
--- assets/utils/handleRenderProyectoGrado.R
+++ assets/utils/handleRenderProyectoGrado.R
@@ -3,6 +3,6 @@ if (!('utils' %in% ls())) { utils <- list() }
 utils$handleRenderInvestigacion<- function(){
     
     ruta <- paste0(getwd(), '/PRINCIPAL-INVESTIGACION.Rmd')
-    rmarkdown::render(ruta)
+    rmarkdown::render(ruta, clean = TRUE)
     
 }
