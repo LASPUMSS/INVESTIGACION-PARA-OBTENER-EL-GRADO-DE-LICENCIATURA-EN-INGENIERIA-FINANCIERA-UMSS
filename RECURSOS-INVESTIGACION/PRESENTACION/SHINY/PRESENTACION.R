782dc35ac57a70a58fee1218770baa582e756f9d
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-10-26 17:59:12 -0600

    UPDATE

--- RECURSOS-INVESTIGACION/PRESENTACION/SHINY/PRESENTACION.R
+++ RECURSOS-INVESTIGACION/PRESENTACION/SHINY/PRESENTACION.R
@@ -15,20 +15,53 @@ ui <- fluidPage(
     includeCSS("RECURSOS-INVESTIGACION/PRESENTACION/SHINY/assets/css/style.css"),
     
     # Application title
-    titlePanel("Proyección de estados financieros por el método de redes neuronales artificiales aplicable al sector financiero de Bolivia"),
-
+    tags$div(class="header-presentacion", checked=TRUE,
+             tags$div(class="header-logo-umss", checked=TRUE,
+                      tags$img(src='https://drive.google.com/uc?export=view&id=1NoTXxCel5n3nbP1xrPLowXDz__3hC-zs', width='100px',height='100px')
+             ),
+             tags$div(class="header-title-proyecto", checked=TRUE,
+   
+                       tags$text('PROYECCIÓN DE ESTADOS FINANCIEROS POR EL MÉTODO DE REDES NEURONALES ARTIFICIALES APLICABLE AL SECTOR FINANCIERO DE BOLIVIA')
+             ),
+             tags$div(class="header-logo-fce", checked=TRUE,
+                      
+                      tags$img(src='https://drive.google.com/uc?export=view&id=1NSE4R6ae1MQpFtBdos5-R-NWTSDXYPpA', width='100px',height='100px')
+             )
+    ),
+    
     # Sidebar with a slider input for number of bins 
     navbarPage("",
-               tabPanel("PERFIL"),
+               navbarMenu("PERFIL DE INVESTIGACION",
+                          tabPanel("PLANTEAMIENTO DEL PROBLEMA"),
+                          tabPanel("FORMULACIÓN DEL PROBLEMA CENTRAL"),
+                          tabPanel("JUSTIFICACIÓN"),
+                          tabPanel("ALCANCE Y DELIMITACIÓN"),
+                          tabPanel("OBJETIVOS DE LA INVESTIGACIÓN"),
+                          tabPanel("HIPÓTESIS"),
+                          tabPanel("MARCO METODOLÓGICO")
+               ),
+               
                tabPanel("MARCO TEORICO"),
+               
                navbarMenu("DIAGNOSTICO FINANCIERO",
                           tabPanel("PRECISIONES DEL DIAGNOSTICO"),
-                          tabPanel("CALCULO DE INDICADORES", uiOutput("diagnosticoCalculoIndicadoes"))
+                          tabPanel("CALCULO DE INDICADORES", uiOutput("diagnosticoCalculoIndicadoes")),
+                          tabPanel("DEFINICIÓN DE RANGOS Y LÍMITES DE LOS INDICADORES CAMEL"),
+                          tabPanel("PONDERACIÓN DE ELEMENTOS CAMEL"),
+                          tabPanel("RESULTADOS CAMEL DEL SISTEMA FINANCIERO")
                ),
-               tabPanel("PRONOSTICO DE ESTADOS FINANCIEROS POR NN"),
+               
+               navbarMenu("PROYECCION DE ESTADOS FINANCIEROS",
+                          tabPanel("PRECISIONES DE LOS PRONÓSTICOS DE ESTADOS FINANCIEROS"),
+                          tabPanel("ENTRENAMIENTO DE MODELOS, PROYECCIONES Y SIMULACIONES"),
+                          tabPanel("EVALUACIÓN DE DATOS AJUSTADOS DE MODELOS"),
+                          tabPanel("EVALUACIÓN DE DATOS PROYECTADOS DE MODELOS"),
+                          tabPanel("APLICACIÓN DE METODOLOGÍA CAMEL SOBRE DATOS PROYECTADOS")
+               ),
+               
                navbarMenu("CONCLUSIONES Y RECOMENDACIONES",
-                          tabPanel("OBJETIVOS", uiOutput("conclusionesObjetivos")),
-                          tabPanel("GENERAL", uiOutput("conclusionesGeneral"))
+                          tabPanel("CONCLUSIÓN OBJETIVOS", uiOutput("conclusionesObjetivos")),
+                          tabPanel("CONCLUSIÓN GENERAL", uiOutput("conclusionesGeneral"))
                ),
     )
 )
@@ -36,23 +69,27 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
+    rootMainDir <- 'RECURSOS-INVESTIGACION/PRESENTACION/SHINY/'
     
     # DIAGNOSTICO
+    
+    # DIAGNOSTICO
+    
     output$diagnosticoCalculoIndicadoes <- renderUI({
-        HTML(markdown::markdownToHTML(knitr::knit("RECURSOS-INVESTIGACION/PRESENTACION/SHINY/DIAGNOSTICO/02-CALCULO-INDICADORES_PRESENTACION.Rmd", quiet = TRUE), fragment.only=TRUE))
-        
+        rootDirFile <- "DIAGNOSTICO/02-CALCULO-INDICADORES_PRESENTACION.Rmd"
+        HTML(markdown::markdownToHTML(knitr::knit(paste0(rootMainDir,rootDirFile), quiet = TRUE), fragment.only=TRUE))
     })
     
     
     # CONCLUSIONES
     output$conclusionesObjetivos <- renderUI({
-        HTML(markdown::markdownToHTML(knitr::knit("RECURSOS-INVESTIGACION/PRESENTACION/SHINY/CONCLUSIONES/CONCLUSIONES_OBJETIVOS_PRESENTACION.Rmd", quiet = TRUE), fragment.only=TRUE))
-        
+        rootDirFile <- "CONCLUSIONES/CONCLUSIONES_OBJETIVOS_PRESENTACION.Rmd"
+        HTML(markdown::markdownToHTML(knitr::knit(paste0(rootMainDir,rootDirFile), quiet = TRUE), fragment.only=TRUE))
     })
     
     output$conclusionesGeneral <- renderUI({
-        HTML(markdown::markdownToHTML(knitr::knit("RECURSOS-INVESTIGACION/PRESENTACION/SHINY/CONCLUSIONES/CONCLUSIONES_GENERAL_PRESENTACION.Rmd", quiet = TRUE), fragment.only=TRUE))
-        
+        rootDirFile <- "CONCLUSIONES/CONCLUSIONES_GENERAL_PRESENTACION.Rmd"
+        HTML(markdown::markdownToHTML(knitr::knit(paste0(rootMainDir,rootDirFile), quiet = TRUE), fragment.only=TRUE))
     })
     
 }
