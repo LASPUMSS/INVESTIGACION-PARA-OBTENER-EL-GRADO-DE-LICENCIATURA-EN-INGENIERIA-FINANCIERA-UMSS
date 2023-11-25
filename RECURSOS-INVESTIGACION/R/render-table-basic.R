32351fe5d4cd41daa0153f40369e3a2638cbb75e
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-11-25 14:36:48 -0600

    UPDATE

--- RECURSOS-INVESTIGACION/R/render-table-basic.R
+++ RECURSOS-INVESTIGACION/R/render-table-basic.R
@@ -33,6 +33,38 @@ renderTableBasic <- function(dat, captionTable='tabla', headerUpper=TRUE, fontSi
     return(result)
 }
 
+renderTableBasic2 <- function(dat, captionTable='tabla', headerUpper=TRUE, fontSize=8) {
+    
+    require(knitr)
+    require(kableExtra)
+    require(dplyr)
+    
+    # Formato Titulos
+    names(dat) <- gsub('_',' ', names(dat))
+    
+    if (headerUpper ) {
+        names(dat) <- toupper(names(dat))
+    } else{
+        names(dat) <- gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(names(dat)), perl=TRUE)    
+    }
+    
+    if ('TIPO DE ENTIDAD' %in% names(dat)) {
+        dat[,'TIPO DE ENTIDAD'] <- gsub('_',' ', dat[,'TIPO DE ENTIDAD'])
+    }
+    
+    
+    result <- 
+        kbl(dat, 
+            longtable = T, 
+            booktabs = T, 
+            caption = "Limites para indicadores CAMEL") %>%
+        kable_styling(latex_options = c("hold_position", "repeat_header"),
+                      font_size = fontSize,
+                      repeat_header_text = "(Continuación)")
+    
+    return(result)
+}
+
 
 renderTableBasicSinCaption <- function(dat, titulo='titulo' ,headerUpper=TRUE, fontSize=8) {
     
--- RECURSOS-INVESTIGACION/R/zzz_render_table_imgs.R
+++ RECURSOS-INVESTIGACION/R/zzz_render_table_imgs.R
@@ -1,5 +1,8 @@
 library(gridExtra)
-df <- datIdsNamesCamelInds
-png("test.png", height = 25*nrow(df), width = 200*ncol(df))
-grid.table(df)
-dev.off()
+df <- renderTableBasic2(resultadosARIMA,captionTable = 'Regresores y medias moviles del modeo ARIMA') %>% 
+    footnote(general = 'NaN indica que uno de los series de tiempo es 0 en todos sus elementos, donde el R2 devuelve una indeterminación.',
+             general_title = 'NOTA: ' )
+df %>% 
+    kable_styling(font_size = 50) %>% 
+    save_kable(file = "table_2.png", zoom = 3)
+
